78c78ba59dbcefbc5084bfd7beb99dcbd883daea
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-03-30 15:08:44 -0700

    gitin started

---
+++ index.js
@@ -0,0 +1,2 @@
+//short easy liri app
+
---
+++ keys.js
@@ -0,0 +1,19 @@
+
+
+// twitter keys
+// var twitterKeys = {
+//   consumer_key: 'xVgCkMbjB4UAVkj8nqxVYgBI4',
+//   consumer_secret: 'IDiT5pqfjuvwUOzKBZSFCK79lgK4ow7aQmB8YoR9wB107tkYoT',
+//   access_token_key: '952702447360733190-CCGEam6K2ZgGNbpL03YKsr1rDQN1qsa',
+//   access_token_secret: '0cPkgnYDw8FcUZ3Bx8ZXX9To850cCtayVlbFuaS1XeKuC',
+// }
+// module.exports = twitterKeys;
+/*
+var spotify = new Spotify({
+  id: 'f8587ba41adc4933a72eee1d6eade2e2',
+  secret: 'cb18f68dfd434c90a374f523cb9a117b'
+});
+module.exports = spotify;
+*/
+
+//bands in town look at my own key and test it on my bandsInTown project.

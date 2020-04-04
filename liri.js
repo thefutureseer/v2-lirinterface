e7ad2d614b1523d6c55713725fbec80957685376
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-04-03 17:39:09 -0700

    add Axios get no bugs. clean up

--- liri.js
+++ liri.js
@@ -1,11 +1,15 @@
 //liri app
+const axios = require("axios");
+
+// var fs = require("fs");
+
 // require("dotenv").config();
 // const keys = require("./keys");
 
 // //access keys info
 // var spotify = new Spotify(keys.spotify);
 
-//object with all the commands to iterate through
+//all the commands to iterate through
 var comds = [
    `concert-this`,
 
@@ -16,10 +20,31 @@ var comds = [
    `do-what-it-says`
 ]
 //this will take the comand from the comand line
-for (var key in comds){
+for (var i in comds){
 
-  if (process.argv[2] == comds[key]) {
+  if (process.argv[2] == comds[i]) {
    console.log("great music goes here ____>")
 
   }
 }
+
+var nameHere = process.argv[3];
+function getBandsInTown(nameHere) {
+// var keyurl = "?app_id=codingbootcamp";
+// var queryUrl = "https://rest.bandsintown.com/artists/" + nameHere + keyurl;
+
+axios.get("https://rest.bandsintown.com/artists/" + nameHere + "?app_id=codingbootcamp").then(response => {
+  console.log(response);
+  
+}).catch(error => {
+    console.log( error.response);
+  });
+}
+getBandsInTown(nameHere);
+
+//DELETE THIS AND USE AXIOS INSTEAD OF AJAX..
+
+//   fs.writeFile("random.txt", res, function(err) {
+//     if (err) { return console.log(err); }
+//   });
+  

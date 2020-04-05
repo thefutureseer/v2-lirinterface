f3e6314ddaf890414e2bc22254267bcf412b3e59
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-04-04 21:08:12 -0700

    added to dotenv

--- keys.js
+++ keys.js
@@ -5,11 +5,3 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-// twitter keys
-// var twitterKeys = {
-//   consumer_key: 'xVgCkMbjB4UAVkj8nqxVYgBI4',
-//   consumer_secret: 'IDiT5pqfjuvwUOzKBZSFCK79lgK4ow7aQmB8YoR9wB107tkYoT',
-//   access_token_key: '952702447360733190-CCGEam6K2ZgGNbpL03YKsr1rDQN1qsa',
-//   access_token_secret: '0cPkgnYDw8FcUZ3Bx8ZXX9To850cCtayVlbFuaS1XeKuC',
-// }
-// module.exports = twitterKeys;

6041b487815f22d3316874f923240f4613ad0593
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-04-05 19:54:52 -0700

    add switch and function closure

--- liri.js
+++ liri.js
@@ -1,35 +1,50 @@
-//liri app
+//liri app dependencies
 const axios = require("axios");
 const Spotify = require("node-spotify-api");
-
 var fs = require("fs");
-
 require("dotenv").config();
 const keys = require("./keys");
 
 //Spotify info
 var spotify = new Spotify(keys.spotify);
 
-
-//all the commands to iterate through
-var comds = [
-  `concert-this`,
-  
-  `spotify-this-song`,
-  
-  `movie-this`,
-  
-  `do-what-it-says`
-];
-
+//this will take the comand from the comand line
+var choys = process.argv[2];
 var nameHere = process.argv[3];
+
+//all the commands to iterate through with a switch 
+
+switch (choys) {
+ case `concert-this`:
+  getBandsInTown();
+  break;
+  
+ case `spotify-this-song`:
+  spotifyMe();
+   break;
+  
+ case `movie-this`:
+   movieLookUp();
+  break;
+
+ case `do-what-it-says`:
+   doThis();
+   break;
+};
+
+
 
 function getBandsInTown(nameHere) {
   // var keyurl = "?app_id=codingbootcamp";
   // var queryUrl = "https://rest.bandsintown.com/artists/" + nameHere + keyurl;
   
   axios.get("https://rest.bandsintown.com/artists/" + nameHere + "?app_id=codingbootcamp").then(response => {
+
+    // Name of the venue
     console.log(response);
+    // Venue location
+
+    // Date of the Event (use moment to format this as "MM/DD/YYYY")
     
   }).catch(error => {
     
@@ -52,30 +67,32 @@ function getBandsInTown(nameHere) {
   });
 }
 
-//this will take the comand from the comand line
-var choys = process.argv[2];
-for (var i in comds){
+function spotifyMe() {
+
   //write the choice to random text file
-  fs.appendFile("random.txt", " " + choys + " ", function(err) {
+  fs.appendFile("random.txt", "Previously played " + nameHere + " ", function(err) {
     if (err) {
       return console.log(err);
     }
     console.log("Choice logged to the random text file");
   });
   
-  if (choys == comds[0]) {
-    getBandsInTown(nameHere);
-    console.log("Concert command goes here ____>")
-
-  } else if (choys === comds[1]) {
     //spitify search query
       spotify.request("https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx")
        .then(function( data) {
-          console.log(data);
-          console.log("Que Spotify song now")
+
+          //Artist name     
+          console.log("Artist name "+ data.name);
+          console.log("Track number "+ data.track_number);
+          // A preview link of the song from Spotify
+          console.log("Album preview link to SPotify "+ data.external_urls);
+          // The album that the song is from
+          //console.log(data.);
+
+
+     
 
        }).catch(function(err) { 
           console.error("Error occurred" + err);
        });
-  }
 }

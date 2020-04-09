49131ce89c7c2f4d6093bebe12bc6edd82e233ae
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-04-08 20:22:40 -0700

    finished bands in town

--- keys.js
+++ keys.js
@@ -1,5 +1,4 @@
 console.log("This is loaded");
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -7,6 +7,8 @@ const Spotify = require("node-spotify-api");
 const keys = require("./keys");
 //Import axios 
 const axios = require("axios");
+//Import moment for date 
+const moment = require("moment");
 //Import File System
 //var fs = require("fs");
 
@@ -24,61 +26,71 @@ var spotifyMe = function(songHere) {
   
   //spitify search query
   spotify.search(
-    
-    {
-      type: "track", 
-      query: songHere ?  songHere : "The Sign",
-      limit: 3
-          
-    }, 
-    function(err, data) {
+  {
+    type: "track", 
+    query: songHere ?  songHere : "The Sign",
+    limit: 3
+        
+  }, 
+  function(err, data) {
 
-      if (err) {
-        console.log("Error occurred" + err);
-        return;
-      }
+    if (err) {
+      console.log("Error occurred" + err);
+      return;
+    }
 
-      var songs = data.tracks.items;
+    var songs = data.tracks.items;
 
-      for (var i = 0; i < songs.length; i++) {
-        //Artist name     
-        console.log("Artist name "+ songs[i].artists.map(artistName));
-        //Song name
-        console.log("Song name "+ songs[i].name);
-        //Track number
-        console.log("Track number "+ songs[i].track_number);
-        //A preview link of the songs from Spotify
-        console.log("Album preview link to SPotify "+ songs[i].preview_url);
-        //The album that the songs is from
-        console.log("Album: " + songs[i].album.name);
-        // console.log(r/n);
-        console.log('---------------------------------------------------')
-        //console.log(songs);
-      }  
+    for (var i = 0; i < songs.length; i++) {
+      //Artist name     
+      console.log("Artist name "+ songs[i].artists.map(artistName));
+      //Song name
+      console.log("Song name "+ songs[i].name);
+      //Track number
+      console.log("Track number "+ songs[i].track_number);
+      //A preview link of the songs from Spotify
+      console.log("Album preview link to SPotify "+ songs[i].preview_url);
+      //The album that the songs is from
+      console.log("Album: " + songs[i].album.name);
+      // console.log(r/n);
+      console.log('---------------------------------------------------')
+      //console.log(songs);
+    }  
    }
  );
 };
 
 //BANDS IN TOWN
-function getBandsInTown(bandNameHere) {
-  // var keyurl = "?app_id=codingbootcamp";
-  // var queryUrl = "https://rest.bandsintown.com/artists/" + bandNameHere + keyurl;
-  
-  axios.get("https://rest.bandsintown.com/artists/" + bandNameHere + "?app_id=codingbootcamp")
-   .then(response => {
+var getBandsInTown = function(bandNameHere) {
 
-    // Name of the venue
-    console.log(response);
-    // Venue location
-    console.log(response);
-    // Date of the Event (use moment to format this as "MM/DD/YYYY")
-    console.log(response);
-   }).catch(error => {
-    if (error.response) {
-      //If the request was made and the server responded with a status code outside of 2xx
-      console.log( error.response.name);
+  var queryUrl = "https://rest.bandsintown.com/artists/" + bandNameHere + "/events?app_id=codingbootcamp";
+  axios.get(queryUrl)
+   .then( function(response) {
+     var jsonData = response.data;
+
+     if (!jsonData.length) {
+       console.log("No results for " + bandNameHere);
+       return;
+     }
+
+     for (var i = 0; i < jsonData.length; i++) {
+      var show = jsonData[i];
+       // Name of the city where venue is
+      console.log( show.venue.city + " "
+       //region or country and venue name.
+      + (show.venue.region || show.venue.country) + " at " 
+      + show.venue.name +
+       // Date of show.(use moment to format this as "MM/DD/YYYY")
+      " Show Date: " + moment(show.datetime).format("MM/DD/YYYY") );
+     }
+   })
+   .catch(error => {
+     if (error.response) {
+       //If the request was made and the server responded with a status code outside of 2xx
+       console.log( error.response.name);
       
-    } else if (error.request) {
+     } 
+    else if (error.request) {
       //If request was made but no response was recieved. 
       //"Error.request" is an object that comes back with details pertaining to the error 
       console.log(error.request);
@@ -87,8 +99,8 @@ function getBandsInTown(bandNameHere) {
       //If something happened when setting up the request that triggered the error
       console.log("Error", error.message);
     }
-     console.log(error.config);
-  });
+      console.log(error.config);
+   });
 }
 
 //all the commands to iterate through with a switch 
